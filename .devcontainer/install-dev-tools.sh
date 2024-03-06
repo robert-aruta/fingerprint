5da9419464db0643a5549894fdf8ca141e4dd63f
Brian Lovell <lovell@itee.uq.edu.au>
2024-03-06 12:45:52 +1000

    Added -y

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,7 +2,7 @@
 # pip3 cache purge
 # sudo apt-get autoremove -y
 # sudo apt-get clean
-
 pip install opencv_contrib_python
-sudo apt-get install libgl1
+sudo apt-get update
+sudo apt-get install -y libgl1
 sudo ldconfig
