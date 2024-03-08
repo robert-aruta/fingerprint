2dea53687875fb84085c00eba232eff380d43fde
Brian Lovell <lovell@itee.uq.edu.au>
2024-03-08 17:25:23 +1000

    Works with Windows 11 and Ubuntu kernel

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,7 +2,6 @@
 # pip3 cache purge
 # sudo apt-get autoremove -y
 # sudo apt-get clean
-pip install opencv_contrib_python
 sudo apt-get update
 sudo apt-get install -y libgl1
-sudo ldconfig
+pip install opencv_contrib_python
