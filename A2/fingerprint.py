f49b0e12b5c88bf9cde211e98e01f994d36cc303
Brian Lovell <lovell.brian@gmail.com>
2024-04-10 11:06:23 +0000

    forced directory change on jupyter notebook to match the working directory for f*.py; and progressing on making the back end

--- A2/fingerprint.py
+++ A2/fingerprint.py
@@ -1,14 +1,4 @@
-
 from os import path
-import utils  # Run utils.py for helper functions
-import subprocess
-import math
-import os
-import numpy as np
-import cv2 as cv
-import matplotlib.pyplot as plt
-from utils import *
-from ipywidgets import interact
 
 if not path.exists('utils.py'): # If running on colab: the first time download and unzip additional files
     
@@ -17,6 +7,19 @@ if not path.exists('utils.py'): # If running on colab: the first time download a
 
     # Run the unzip command to extract the contents
     subprocess.run(['unzip', 'files.zip'])
+    
+import utils  # Run utils.py for helper functions
+import math
+import os
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+from utils import *
+from ipywidgets import interact
+
+
+
 
 # Read Fingerprints in the fingerprint subdirectory
 def read_fingerprints():
@@ -28,11 +31,11 @@ def read_fingerprints():
     for filename in os.listdir(directoryPath):
         
         filepath = os.path.join(directoryPath, filename)
-        image = cv.imread(filepath)
+        image = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
 
         if fingerprints is not None:
 
-            fingerprints.append(image)
+            fingerprints.append((image, filename))
 
         else:
             print("Failed to Read image of %s\n" % filename)
@@ -46,28 +49,67 @@ def calc_sobel(fingerprints):
     GyList = []
 
     # Calculate the local gradient for each fingerprins in the directory 
-    for f in fingerprints:
+    for image, filename in fingerprints:
         
-        Gx = cv.Sobel(f, cv.CV_32F, 1, 0)
+        Gx = cv.Sobel(image, cv.CV_32F, 1, 0)
         Gx = Gx**2
         GxList.append(Gx)
         
-        Gy = cv.Sobel(f, cv.CV_32F, 0, 1)
+        Gy = cv.Sobel(image, cv.CV_32F, 0, 1)
         Gy = Gy**2
         GyList.append(Gy)
     
-    Gm = [np.sqrt(x + y) for x, y in zip(GxList, GyList)]
+    GmList = [np.sqrt(x + y) for x, y in zip(GxList, GyList)]
     
-    return Gm
+    return GxList, GyList, GmList
 
-# # Get the current working directory
-# current_directory = os.getcwd()
 
-# # Print the current working directory
-# print("Current Working Directory:", current_directory)
+def sum_Gm(GmList):
     
-readFingerprints = read_fingerprints()
-fingerprintSobel = calc_sobel(readFingerprints)
-cv.imshow('Sobel', fingerprintSobel[0])
-# for i, fpS in enumerate(fingerprintSobel):
-#     cv.imshow('Sobel %d' % i, fpS)
+    sumList = []
+    
+    for g in GmList:
+        
+        sumGm = cv.boxFilter(g, -1, (25, 25), normalize = False)
+        sumList.append(sumGm)
+        
+    return sumList
+
+def threshold_mask(sumList):
+    
+    thresholdList = []
+    maskList = []
+    
+    for i in sumList:
+        
+        threshold = i.max() * 0.2
+        mask = cv.threshold(i, threshold, 255, cv.THRESH_BINARY)[1].astype(np.uint8)
+        maskList.append(mask)
+        
+    return maskList
+
+#def orientation()
+def print_wd():
+    # Get the current working directory
+    currentDirectory = os.getcwd()
+
+    # Print the current working directory
+    print(currentDirectory)
+
+def main():
+    
+    print_wd()
+    readFingerPrints = read_fingerprints()
+    GxList, GyList, GmList = calc_sobel(readFingerPrints)
+    Gx = GxList[0]
+    _, filename = readFingerPrints[0]
+    sumGm = sum_Gm(GmList)
+    thres = threshold_mask(sumGm)
+    plt.imshow(thres[0], cmap='gray')  # You can specify the colormap to 'gray' for grayscale images
+    plt.title(f'Integral Gradient Magnitude of {filename}')
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
+
+
